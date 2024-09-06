9c570c2cdbe378f1288aa0d9fed8bb0b37103169
Victor Hugo <victorhugobbpereira02@gmail.com>
2024-09-06 13:55:04 -0300

    inicio da função de postagem

--- lib/src/views/navigation/newpost/newpost_page.dart
+++ lib/src/views/navigation/newpost/newpost_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:unijobs/src/components/textformfields/textformfield_component.dart';
 import 'package:unijobs/src/responsive/display_responsive.dart';
 import 'package:unijobs/src/theme/theme_color.dart';
@@ -19,149 +21,233 @@ class _NewpostPageState extends State<NewpostPage> with ValidationMixinClass {
   final TextEditingController salaryController = TextEditingController();
   final TextEditingController localController = TextEditingController();
   final TextEditingController enterpriseController = TextEditingController();
+  final GlobalKey<FormState> _keyForm = GlobalKey();
+  final fireUid = FirebaseAuth.instance.currentUser!.uid;
+  final supabase = Supabase.instance.client;
+
+  Future sendPost({
+    required String title,
+    required String subtitle,
+    required String local,
+    required String salary,
+    required String description,
+    required String period,
+    required String enterprise,
+  }) async {
+    try {
+      final response = await supabase.from('postagem').insert({
+        'id_fk_usuario': fireUid.toString(),
+        'titulo': title,
+        'subtitulo': subtitle,
+        'local': local,
+        'salario': salary,
+        'descricao': description,
+        'periodo': period,
+        'empresa': enterprise,
+      });
+      return 'Postagem enviada! $response';
+    } catch (error) {
+      return 'Erro ao criar usuário: $error';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final responsive = Responsive(context);
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              barrierDismissible: true,
-              builder: (BuildContext context) {
-                return Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Center(
-                    child: Material(
-                      child: Card(
-                        child: SingleChildScrollView(
-                          child: Container(
-                            padding: const EdgeInsets.all(40.0),
-                            width: (responsive.isMobile || responsive.isTablet)
-                                ? MediaQuery.of(context).size.width *
-                                    0.9 // 90% da largura da tela
-                                : 700,
-                            child: Form(
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  TextFormFieldComponent(
-                                    iconP: const Icon(Icons.text_fields_rounded),
-                                    controller: titleController,
-                                    inputType: TextInputType.text,
-                                    obscure: false,
-                                    labelText: 'Título',
-                                    validator: isNotEmpyt,
-                                  ),
-                                  const SizedBox(height: 15),
-                                  TextFormFieldComponent(
-                                    iconP: const Icon(Icons.text_fields_rounded),
-                                    controller: subtitleController,
-                                    inputType: TextInputType.text,
-                                    obscure: false,
-                                    labelText: 'Subtítulo',
-                                    validator: isNotEmpyt,
-                                  ),
-                                  const SizedBox(height: 15),
-                                  TextFormFieldComponent(
-                                    iconP: const Icon(Icons.location_on),
-                                    controller: localController,
-                                    inputType: TextInputType.text,
-                                    obscure: false,
-                                    labelText: 'Local',
-                                    validator: isNotEmpyt,
-                                  ),
-                                  const SizedBox(height: 15),
-                                  TextFormFieldComponent(
-                                    iconP: const Icon(Icons.monetization_on),
-                                    controller: salaryController,
-                                    inputType: TextInputType.text,
-                                    obscure: false,
-                                    labelText: 'Salário',
-                                    validator: isNotEmpyt,
-                                  ),
-                                  const SizedBox(height: 15),
-                                  TextFormFieldComponent(
-                                    iconP: const Icon(Icons.date_range),
-                                    controller: periodController,
-                                    inputType: TextInputType.text,
-                                    obscure: false,
-                                    labelText: 'Período',
-                                    validator: isNotEmpyt,
-                                  ),
-                                  const SizedBox(height: 15),
-                                  TextFormFieldComponent(
-                                    iconP: const Icon(Icons.apartment),
-                                    controller: enterpriseController,
-                                    inputType: TextInputType.text,
-                                    obscure: false,
-                                    labelText: 'Nome empresa',
-                                    validator: isNotEmpyt,
-                                  ),
-                                  const SizedBox(height: 25),
-                                  TextField(
-                                    keyboardType: TextInputType.multiline,
-                                    controller: descriptionController,
-                                    minLines: 10,
-                                    maxLines: null,
-                                    decoration: const InputDecoration(
-                                      hintText: 'Descrição',
-                                      border: OutlineInputBorder(),
-                                    ),
-                                  ),
-                                  Container(
-                                    padding: const EdgeInsets.all(15),
-                                    height: 80,
-                                    width: responsive.isMobile
-                                        ? double.infinity
-                                        : 450,
-                                    child: ElevatedButton(
-                                      onPressed: () {},
-                                      style: ElevatedButton.styleFrom(
-                                        elevation: 0.0,
-                                        padding: const EdgeInsets.symmetric(
-                                          vertical: 15,
-                                          horizontal: 10,
-                                        ),
-                                        backgroundColor:
-                                            ColorSchemeManagerClass.colorSecondary,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(5),
-                                        ),
-                                        foregroundColor:
-                                            ColorSchemeManagerClass.colorWhite,
+      body: Column(
+        children: [
+          Text(fireUid),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('teste curto'),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  barrierDismissible: true,
+                  builder: (BuildContext context) {
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Center(
+                        child: Material(
+                          child: Card(
+                            child: SingleChildScrollView(
+                              child: Container(
+                                padding: const EdgeInsets.all(40.0),
+                                width:
+                                    (responsive.isMobile || responsive.isTablet)
+                                        ? MediaQuery.of(context).size.width *
+                                            0.9 // 90% da largura da tela
+                                        : 700,
+                                child: Form(
+                                  key: _keyForm,
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      TextFormFieldComponent(
+                                        iconP: const Icon(
+                                            Icons.text_fields_rounded),
+                                        controller: titleController,
+                                        inputType: TextInputType.text,
+                                        obscure: false,
+                                        labelText: 'Título',
+                                        validator: isNotEmpyt,
                                       ),
-                                      child: const Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Text('Postar vaga'),
-                                          SizedBox(width: 10),
-                                          Icon(Icons.send)
-                                        ],
+                                      const SizedBox(height: 15),
+                                      TextFormFieldComponent(
+                                        iconP: const Icon(
+                                            Icons.text_fields_rounded),
+                                        controller: subtitleController,
+                                        inputType: TextInputType.text,
+                                        obscure: false,
+                                        labelText: 'Subtítulo',
+                                        validator: isNotEmpyt,
                                       ),
-                                    ),
+                                      const SizedBox(height: 15),
+                                      TextFormFieldComponent(
+                                        iconP: const Icon(Icons.location_on),
+                                        controller: localController,
+                                        inputType: TextInputType.text,
+                                        obscure: false,
+                                        labelText: 'Local',
+                                        validator: isNotEmpyt,
+                                      ),
+                                      const SizedBox(height: 15),
+                                      TextFormFieldComponent(
+                                        iconP:
+                                            const Icon(Icons.monetization_on),
+                                        controller: salaryController,
+                                        inputType: TextInputType.text,
+                                        obscure: false,
+                                        labelText: 'Salário',
+                                        validator: isNotEmpyt,
+                                      ),
+                                      const SizedBox(height: 15),
+                                      TextFormFieldComponent(
+                                        iconP: const Icon(Icons.date_range),
+                                        controller: periodController,
+                                        inputType: TextInputType.text,
+                                        obscure: false,
+                                        labelText: 'Período',
+                                        validator: isNotEmpyt,
+                                      ),
+                                      const SizedBox(height: 15),
+                                      TextFormFieldComponent(
+                                        iconP: const Icon(Icons.apartment),
+                                        controller: enterpriseController,
+                                        inputType: TextInputType.text,
+                                        obscure: false,
+                                        labelText: 'Nome empresa',
+                                        validator: isNotEmpyt,
+                                      ),
+                                      const SizedBox(height: 25),
+                                      TextFormField(
+                                        keyboardType: TextInputType.multiline,
+                                        controller: descriptionController,
+                                        minLines: 10,
+                                        maxLines: null,
+                                        decoration: const InputDecoration(
+                                          hintText: 'Descrição',
+                                          border: OutlineInputBorder(),
+                                        ),
+                                        validator: isNotEmpyt,
+                                      ),
+                                      Container(
+                                        padding: const EdgeInsets.all(15),
+                                        height: 80,
+                                        width: responsive.isMobile
+                                            ? double.infinity
+                                            : 450,
+                                        child: ElevatedButton(
+                                          onPressed: () {
+                                            if (_keyForm.currentState!
+                                                .validate()) {
+                                              String title = titleController
+                                                  .value
+                                                  .toString();
+                                              String subtitle = titleController
+                                                  .value
+                                                  .toString();
+                                              String local = localController
+                                                  .value
+                                                  .toString();
+                                              String salary = salaryController
+                                                  .value
+                                                  .toString();
+                                              String period = periodController
+                                                  .value
+                                                  .toString();
+                                              String enterprise =
+                                                  enterpriseController.value
+                                                      .toString();
+                                              String description =
+                                                  descriptionController.value
+                                                      .toString();
+                                              sendPost(
+                                                description: description,
+                                                enterprise: enterprise,
+                                                local: local,
+                                                period: period,
+                                                salary: salary,
+                                                subtitle: subtitle,
+                                                title: title,
+                                              );
+                                              _keyForm.currentState!.reset();
+                                            }
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                            elevation: 0.0,
+                                            padding: const EdgeInsets.symmetric(
+                                              vertical: 15,
+                                              horizontal: 10,
+                                            ),
+                                            backgroundColor:
+                                                ColorSchemeManagerClass
+                                                    .colorSecondary,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                            foregroundColor:
+                                                ColorSchemeManagerClass
+                                                    .colorWhite,
+                                          ),
+                                          child: const Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Text('Postar vaga'),
+                                              SizedBox(width: 10),
+                                              Icon(Icons.send)
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 );
               },
-            );
-          },
-          child: const Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.add),
-              Text('Nova Postagem'),
-            ],
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.add),
+                  Text('Nova Postagem'),
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
