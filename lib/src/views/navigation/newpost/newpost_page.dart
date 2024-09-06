780dca4173a6acf014766a738b1e913580f93dbb
Aluno <aluno@devisate.local>
2024-09-06 10:53:12 -0300

    Alterações postagens

--- lib/src/views/navigation/newpost/newpost_page.dart
+++ lib/src/views/navigation/newpost/newpost_page.dart
@@ -34,106 +34,116 @@ class _NewpostPageState extends State<NewpostPage> with ValidationMixinClass {
                   padding: const EdgeInsets.all(20.0),
                   child: Center(
                     child: Material(
-                      child: SingleChildScrollView(
-                        child: Container(
-                          padding: const EdgeInsets.all(40.0),
-                          width: (responsive.isMobile || responsive.isTablet)
-                              ? 1.5
-                              : 700,
-                          child: Form(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                TextFormFieldComponent(
-                                  iconP: const Icon(Icons.text_fields_rounded),
-                                  controller: titleController,
-                                  inputType: TextInputType.text,
-                                  obscure: false,
-                                  labelText: 'Título',
-                                  validator: isNotEmpyt,
-                                ),
-                                const SizedBox(height: 15),
-                                TextFormFieldComponent(
-                                  iconP: const Icon(Icons.text_fields_rounded),
-                                  controller: subtitleController,
-                                  inputType: TextInputType.text,
-                                  obscure: false,
-                                  labelText: 'Subtítulo',
-                                  validator: isNotEmpyt,
-                                ),
-                                const SizedBox(height: 15),
-                                TextFormFieldComponent(
-                                  iconP: const Icon(Icons.location_on),
-                                  controller: localController,
-                                  inputType: TextInputType.text,
-                                  obscure: false,
-                                  labelText: 'Local',
-                                  validator: isNotEmpyt,
-                                ),
-                                const SizedBox(height: 15),
-                                TextFormFieldComponent(
-                                  iconP: const Icon(Icons.monetization_on),
-                                  controller: salaryController,
-                                  inputType: TextInputType.text,
-                                  obscure: false,
-                                  labelText: 'Salário',
-                                  validator: isNotEmpyt,
-                                ),
-                                const SizedBox(height: 15),
-                                TextFormFieldComponent(
-                                  iconP: const Icon(Icons.date_range),
-                                  controller: periodController,
-                                  inputType: TextInputType.text,
-                                  obscure: false,
-                                  labelText: 'Período',
-                                  validator: isNotEmpyt,
-                                ),
-                                const SizedBox(height: 15),
-                                TextFormFieldComponent(
-                                  iconP: const Icon(Icons.apartment),
-                                  controller: enterpriseController,
-                                  inputType: TextInputType.text,
-                                  obscure: false,
-                                  labelText: 'Nome empresa',
-                                  validator: isNotEmpyt,
-                                ),
-                                const SizedBox(height: 25),
-                                TextField(
-                                  keyboardType: TextInputType.multiline,
-                                  controller: descriptionController,
-                                  minLines: 10,
-                                  maxLines: null,
-                                  decoration: const InputDecoration(
-                                    hintText: 'Descrição',
-                                    border: OutlineInputBorder(),
+                      child: Card(
+                        child: SingleChildScrollView(
+                          child: Container(
+                            padding: const EdgeInsets.all(40.0),
+                            width: (responsive.isMobile || responsive.isTablet)
+                                ? MediaQuery.of(context).size.width *
+                                    0.9 // 90% da largura da tela
+                                : 700,
+                            child: Form(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  TextFormFieldComponent(
+                                    iconP: const Icon(Icons.text_fields_rounded),
+                                    controller: titleController,
+                                    inputType: TextInputType.text,
+                                    obscure: false,
+                                    labelText: 'Título',
+                                    validator: isNotEmpyt,
                                   ),
-                                ),
-                                Container(
-                                  padding: const EdgeInsets.all(15),
-                                  width: responsive.isMobile
-                                      ? double.infinity
-                                      : 450,
-                                  child: ElevatedButton(
-                                    onPressed: () {},
-                                    style: ElevatedButton.styleFrom(
-                                      elevation: 0.0,
-                                      padding: const EdgeInsets.symmetric(
-                                        vertical: 15,
-                                        horizontal: 10,
-                                      ),
-                                      backgroundColor:
-                                          ColorSchemeManagerClass.colorBlack,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(5),
-                                      ),
-                                      foregroundColor:
-                                          ColorSchemeManagerClass.colorWhite,
-                            
+                                  const SizedBox(height: 15),
+                                  TextFormFieldComponent(
+                                    iconP: const Icon(Icons.text_fields_rounded),
+                                    controller: subtitleController,
+                                    inputType: TextInputType.text,
+                                    obscure: false,
+                                    labelText: 'Subtítulo',
+                                    validator: isNotEmpyt,
+                                  ),
+                                  const SizedBox(height: 15),
+                                  TextFormFieldComponent(
+                                    iconP: const Icon(Icons.location_on),
+                                    controller: localController,
+                                    inputType: TextInputType.text,
+                                    obscure: false,
+                                    labelText: 'Local',
+                                    validator: isNotEmpyt,
+                                  ),
+                                  const SizedBox(height: 15),
+                                  TextFormFieldComponent(
+                                    iconP: const Icon(Icons.monetization_on),
+                                    controller: salaryController,
+                                    inputType: TextInputType.text,
+                                    obscure: false,
+                                    labelText: 'Salário',
+                                    validator: isNotEmpyt,
+                                  ),
+                                  const SizedBox(height: 15),
+                                  TextFormFieldComponent(
+                                    iconP: const Icon(Icons.date_range),
+                                    controller: periodController,
+                                    inputType: TextInputType.text,
+                                    obscure: false,
+                                    labelText: 'Período',
+                                    validator: isNotEmpyt,
+                                  ),
+                                  const SizedBox(height: 15),
+                                  TextFormFieldComponent(
+                                    iconP: const Icon(Icons.apartment),
+                                    controller: enterpriseController,
+                                    inputType: TextInputType.text,
+                                    obscure: false,
+                                    labelText: 'Nome empresa',
+                                    validator: isNotEmpyt,
+                                  ),
+                                  const SizedBox(height: 25),
+                                  TextField(
+                                    keyboardType: TextInputType.multiline,
+                                    controller: descriptionController,
+                                    minLines: 10,
+                                    maxLines: null,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Descrição',
+                                      border: OutlineInputBorder(),
                                     ),
-                                    child: const Text('data'),
                                   ),
-                                ),
-                              ],
+                                  Container(
+                                    padding: const EdgeInsets.all(15),
+                                    height: 80,
+                                    width: responsive.isMobile
+                                        ? double.infinity
+                                        : 450,
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        elevation: 0.0,
+                                        padding: const EdgeInsets.symmetric(
+                                          vertical: 15,
+                                          horizontal: 10,
+                                        ),
+                                        backgroundColor:
+                                            ColorSchemeManagerClass.colorSecondary,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(5),
+                                        ),
+                                        foregroundColor:
+                                            ColorSchemeManagerClass.colorWhite,
+                                      ),
+                                      child: const Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Text('Postar vaga'),
+                                          SizedBox(width: 10),
+                                          Icon(Icons.send)
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
